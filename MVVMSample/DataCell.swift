c982deec90aaf948a0ed863abd253753bbb1e82a
Allen Hsu <allenhsu.taiwan@gmail.com>
2017-03-21 00:08:13 +0800

    first commit

---
+++ MVVMSample/DataCell.swift
@@ -0,0 +1,9 @@
+//
+//  DataCell.swift
+//  MVVMSample
+//
+//  Created by Allen Hsu on 2017/3/20.
+//  Copyright © 2017年 Allen Hsu. All rights reserved.
+//
+
+import Foundation
---
+++ MVVMSample/ViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ViewModel.swift
+//  MVVMSample
+//
+//  Created by Allen Hsu on 2017/3/20.
+//  Copyright © 2017年 Allen Hsu. All rights reserved.
+//
+
+import Foundation
